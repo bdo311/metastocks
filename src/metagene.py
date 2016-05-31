22b36b34173586f1d0b60d53bef8c1f4d6598711
Brian Do <bdo@stanford.edu>
2016-05-31 14:14:45 -0700

    metagenes now work

--- src/metagene.py
+++ src/metagene.py
@@ -3,6 +3,11 @@
 import os
 import sys
 import glob 
+import csv
+import collections
+import numpy as np
+import datetime as dt
+import math
 
 USAGE_STR = """
 # Purpose 
@@ -10,80 +15,161 @@ USAGE_STR = """
 # multiple stock trajectories. 
 
 # Usage 
-# python metagene.py <AGGREGATE_FOLDER_PATH> <OUTPUT_FILE>
+# python metagene.py <AGGREGATE_FOLDER_PATH> <MARKET_CAP_FILE> <OUTPUT_FILE> <START_DATE> <END_DATE> <ONE_DATE>
 
 # Arguments 
 # <AGGREGATE_FOLDER_PATH> Path to the folder aggregating all the daily stock data for 
 # the stocks of interest. 
+# <MARKET_CAP_FILE> Path to the file that has all market cap data
 # <OUTPUT_FILE> Absolute path to the output metagene file. 
+# <START_DATE> Start date of metagene, in form YYYYMMDD
+# <END_DATE> End date of metagene, in form YYYYMMDD
+# <ONE_DATE> Date where all stocks should be normalized to 1, in form YYYYMMDD
 
 # Example
-# setenv AGGREGATE_FOLDER_PATH "/afs/ir.stanford.edu/users/a/k/akma327/stats205/midterm-project/metastocks/data/historical-data/tech"
-# setenv OUTPUT_FILE "/afs/ir.stanford.edu/users/a/k/akma327/stats205/midterm-project/metastocks/data/metagene-output/tech-metagene.txt"
-# python metagene.py $AGGREGATE_FOLDER_PATH $OUTPUT_FILE
+# python metagene.py ../data/historical-data/6-industrial-goods/ ../data/marketcaps.csv ../data/metagene_output/6-metagene.txt 20080101 20151231 20090101
 
 """
 
 METAGENE_FILE_HEADER = "Date,Open,High,Low,Close,Volume\n"
+DATES = [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008]
+WINDOW = 10
+
+def getMarketCap(year, mktcap):
+	if year in mktcap and mktcap[year] != "NA": return mktcap[year]
+	
+	# if the year is out of bounds
+	if year < 2008: return getMarketCap(2008, mktcap)
+	elif year > 2015: return getMarketCap(2015, mktcap)
+	
+	# if the year is within bounds
+	i = DATES.index(year)
+	for j in range(i)[::-1]:  # iterate backwards (forwards in time)
+		cap = mktcap[DATES[j]]
+		if cap != "NA": return cap
+	for j in range(i, len(DATES)):  # iterate forwards (backwards in time)
+		cap = mktcap[DATES[j]]
+		if cap != "NA": return cap		
 
 # Process a single stock time series file 
-def processStockInfo(date_to_stockinfo_dict, stock_file):
-	print(stock_file)
-	f = open(stock_file, 'r')
-	for line in f: 
-		if("Date" in line or line == "\n"): continue
-		linfo = line.split(",")
-		if('' in linfo): continue 
-		Date, Open, High, Low, Close, Volume = linfo[0], float(linfo[1]), float(linfo[2]), float(linfo[3]), float(linfo[4]), int(linfo[5].strip())
-		key = (Open, High, Low, Close, Volume)
-		if(Date not in date_to_stockinfo_dict):
-			date_to_stockinfo_dict[Date] = [key]
-		else:
-			date_to_stockinfo_dict[Date].append(key)
-
-def avgStockStats(stock_info_list):
-	Open, High, Low, Close, Volume = [0]*5
-	nStocks = len(stock_info_list)
-	for s in stock_info_list:
-		Open += s[0]
-		High += s[1]
-		Low += s[2]
-		Close += s[3]
-		Volume += s[4]
-	Open /= nStocks
-	High /= nStocks 
-	Low /= nStocks
-	Close /= nStocks
-	Volume /= nStocks
-	return (str(round(Open,2)), str(round(High,2)), str(round(Low,2)), str(round(Close,2)), str(Volume))
+def processStockInfo(stock_file, mktcap, START, END, ONE):
+	dateToInfo = {}
+	with open(stock_file, 'r') as f:
+		normalizing_prices = []
+		for line in f: 
+			if("Date" in line or line == "\n"): continue
+			linfo = line.split(",")
+			if('' in linfo): continue 
+			
+			datearr = linfo[0].split('-')
+			date = dt.date(int(datearr[0]), int(datearr[1]), int(datearr[2]))
+			if date < START or date > END: continue
+			
+			closeprice = float(linfo[4]) 		
+			if math.fabs((date - ONE).days) < 3: normalizing_prices.append(closeprice)
+			
+			cap = getMarketCap(date.year, mktcap)
+			dateToInfo[date] = [closeprice, cap]
+		
+		# normalize
+		if normalizing_prices == []: return {}  # cannot find a date close enough to normalize by
+		norm_price = np.mean(normalizing_prices)
+		for d in dateToInfo:
+			dateToInfo[d][0] /= norm_price
+			
+	return dateToInfo
 
 
-# Average stocks for a given time point 
-def averageStockInfo(date_to_stockinfo_dict):
-	averageStockInfo = []
-	for key in sorted(date_to_stockinfo_dict.keys()):
-		stock_info_list = date_to_stockinfo_dict[key]
-		avgStats = avgStockStats(stock_info_list)
-		averageStockInfo.append((key, avgStats))
-	return averageStockInfo
+# Average stocks over all time points
+def averageStockInfo(stock_to_dict):
+	averaged_stocks = collections.defaultdict(lambda: ([], []))  # date --> ([prices], [caps])
+	for stock in stock_to_dict:
+		for d in stock_to_dict[stock]:
+			averaged_stocks[d.isoformat()][0].append(stock_to_dict[stock][d][0])
+			averaged_stocks[d.isoformat()][1].append(stock_to_dict[stock][d][1])
+		
+	weighted = {}
+	unweighted = {}
+	for d in averaged_stocks:
+		prices = averaged_stocks[d][0]
+		caps = averaged_stocks[d][1]
+		if len(prices) < 2: continue
+		unweighted[d] = np.mean(prices)		
+		weighted[d] = np.inner(prices, caps)/sum(caps)
+	return (weighted, unweighted)
 
-
-def metagene(AGGREGATE_FOLDER_PATH, OUTPUT_FILE):
-	fw = open(OUTPUT_FILE, 'w')
-	fw.write(METAGENE_FILE_HEADER)
-	date_to_stockinfo_dict = {}
+def readMarketCapFile(fn):
+	stocks_to_mktcap = collections.defaultdict(lambda: {})
+	with open(fn, 'r') as ifile:
+		reader = csv.reader(ifile)
+		reader.next()
+		for row in reader:
+			all_na = True
+			for i in range(len(DATES)):			
+				try: 
+					cap = float(row[2+i].replace(',', ''))
+					all_na = False
+				except:
+					cap = "NA"
+				stocks_to_mktcap[row[0]][DATES[i]] = cap
+			if all_na: del stocks_to_mktcap[row[0]]
+		
+	return stocks_to_mktcap	
+	
+def metagene(AGGREGATE_FOLDER_PATH, MARKET_CAP_FILE, OUTPUT_FILE, START, END, ONE):
+	# Read in files
+	mktcaps = readMarketCapFile(MARKET_CAP_FILE)
 	stock_files = glob.glob(AGGREGATE_FOLDER_PATH + "/*")
+
+	# Get normalized data for each stock over the time period
+	stock_to_dict = {}
 	for stock_file in stock_files:
-		processStockInfo(date_to_stockinfo_dict, stock_file)
-	averagedStockInfo = averageStockInfo(date_to_stockinfo_dict)
-	for s in averagedStockInfo:
-		fw.write(s[0] + "," + s[1][0] + "," + s[1][1] + "," + s[1][2] + "," + s[1][3] + "," + s[1][4] + "\n")
+		stock = os.path.basename(stock_file).replace("_from_google.csv", "")
+		if stock not in mktcaps: 
+			#print "{} not in market caps list".format(stock)
+			continue
+		print stock
+		stock_to_dict[stock] = processStockInfo(stock_file, mktcaps[stock], START, END, ONE)
+	
+	# Make weighted and unweighted metagenes
+	(weighted, unweighted) = averageStockInfo(stock_to_dict)
 
+	with open(OUTPUT_FILE, 'w') as ofile:
+		writer = csv.writer(ofile)
+		row1 = ['date']
+		row1.extend(sorted(weighted.keys()))
+		writer.writerow(row1)
+		
+		row2 = ['weighted']
+		nums = [weighted[x] for x in sorted(weighted.keys())]
+		row2.extend(nums)
+		writer.writerow(row2)
 
+		row3 = ['unweighted']
+		nums = [unweighted[x] for x in sorted(weighted.keys())]
+		row3.extend(nums)
+		writer.writerow(row3)
+		
+def process_date(x, prefix):
+	if len(x) != 8: 
+		print "Date format incorrect for {}".format(x)
+		exit(1)
+	d = dt.date(int(x[:4]), int(x[4:6]), int(x[6:8]))	
+	print "{} date: {}".format(prefix, d)
+	return d
+	
 if __name__ == "__main__":
 	AGGREGATE_FOLDER_PATH = sys.argv[1]
-	OUTPUT_FILE = sys.argv[2]
-	metagene(AGGREGATE_FOLDER_PATH, OUTPUT_FILE)
+	MARKET_CAP_FILE = sys.argv[2]
+	OUTPUT_FILE = sys.argv[3]
+	START = process_date(sys.argv[4], "Start")
+	END = process_date(sys.argv[5], "End")
+	ONE = process_date(sys.argv[6], "Norm")
+	if ONE < START or ONE > END:
+		print "Normalization date is outside the start-end range."
+		exit(1)
+		
+	metagene(AGGREGATE_FOLDER_PATH, MARKET_CAP_FILE, OUTPUT_FILE, START, END, ONE)
 
 
 
